88c2f8ba066034c8fcd1716610682de399396a8f
codyguha <aalo.guha@gmail.com>
2016-11-25 16:04:18 -0800

    facebook survey

--- surveychicken-facebook.js
+++ surveychicken-facebook.js
@@ -213,7 +213,7 @@ function startJoke(incoming) {
 controller.on('message_received', function(bot, incoming) {
 var id = incoming.user
 var text = incoming.text
-  if (!incoming.quick_reply.payload) {
+  if (incoming.quick_reply.attachment.payload) {
     console.log(incoming)
   } else if(incoming.quick_reply.payload){
     var payload = incoming.quick_reply.payload
