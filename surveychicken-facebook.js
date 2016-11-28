b2cac4b91ab0a56fed9449c25c714c4dd2a9fe16
codyguha <aalo.guha@gmail.com>
2016-11-28 11:25:33 -0800

    added location

--- surveychicken-facebook.js
+++ surveychicken-facebook.js
@@ -235,7 +235,7 @@ controller.on('message_received', function(bot, incoming) {
           var city_name = data.results[0].address_components[2].long_name
           saveLocationToMongoDb(id, city_name)
           getChickenNow(incoming, user, city_name)
-          console.log(data)// do something with data
+          console.log(data.results[6].formatted_address)// do something with data
         });
 
     });
